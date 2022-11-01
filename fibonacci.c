933a4894f7df810d1f394c66d89dea2356c9b742
Manteiga <rafael.manteiga@hotmail.com>
2022-10-31 21:28:14 -0300

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int fib(n){
+  if(n==2 || n==3){
+    return 1;
+  }
+  else{
+    return(fib(n-1)+fib(n-2));
+  }
+}
+
+int main() {
+    
+    printf("%d\n",fib(10));
+  
+    return 0;
+}
