a7ee9f444d0212e6a8f95cfa46c2b36b0cdfa290
Manteiga <rafael.manteiga@hotmail.com>
2022-11-03 19:41:10 -0300

    Add files via upload

---
+++ Lista 1/5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <stdbool.h>
+
+bool primo(int n){
+	
+	int k;
+	
+	for(k=2;k<n;k++){
+		if (n%k==0){
+			return false;	
+		}
+	}
+	return true;
+}
+
+int main(int argc, char *argv[]) {
+
+	setlocale(LC_ALL,"portuguese");
+	
+	int n,k;
+	
+	printf("Digite um número: ");
+	scanf("%d",&n);
+	
+	printf("\nNúmeros primos até %d: ",n);
+	
+	for(k=2;k<n;k++){
+		if(primo(k)){
+		printf("%d ",k);
+		}
+	}
+	
+	getch();
+	return 0;
+}
