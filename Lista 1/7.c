d70fd57b209f36553e66735ded05ce4b16c0b2ee
Manteiga <rafael.manteiga@hotmail.com>
2022-11-03 20:49:45 -0300

    Add files via upload

---
+++ Lista 1/7.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int k=0;
+
+int printlista(int n){
+	
+	printf("%d ",k);
+	if(n>k){
+		k++;
+		return printlista(n);
+	}	
+	else{
+		return 0;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	
+	setlocale(LC_ALL,"portuguese");
+	
+	int n;
+	
+	printf("Digite um número: ");
+	scanf("%d",&n);	
+	printf("\nNúmeros de 0 até %d em ordem crescente: ",n);
+	printlista(n);
+	
+	getch();
+	return 0;
+}
