447e56efd61e8b3ce702f3a098707b995ea4e8f0
Manteiga <rafael.manteiga@hotmail.com>
2022-11-04 02:13:25 -0300

    Update 5.c

--- Lista 1/5.c
+++ Lista 1/5.c
@@ -3,35 +3,35 @@
 #include <locale.h>
 #include <stdbool.h>
 
-bool primo(int n){
-	
-	int k;
-	
-	for(k=2;k<n;k++){
-		if (n%k==0){
-			return false;	
-		}
-	}
-	return true;
+bool primo(int n) {
+
+  int k;
+
+  for (k = 2; k < n; k++) {
+    if (n % k == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char * argv[]) {
 
-	setlocale(LC_ALL,"portuguese");
-	
-	int n,k;
-	
-	printf("Digite um número: ");
-	scanf("%d",&n);
-	
-	printf("\nNúmeros primos até %d: ",n);
-	
-	for(k=2;k<n;k++){
-		if(primo(k)){
-		printf("%d ",k);
-		}
-	}
-	
-	getch();
-	return 0;
+  setlocale(LC_ALL, "portuguese");
+
+  int n, k;
+
+  printf("Digite um nÃºmero: ");
+  scanf("%d", & n);
+
+  printf("\nNÃºmeros primos atÃ© %d: ", n);
+
+  for (k = 2; k < n; k++) {
+    if (primo(k)) {
+      printf("%d ", k);
+    }
+  }
+
+  getch();
+  return 0;
 }
