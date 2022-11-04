01b20cc38d39a3674d7ecc9171f3345c4a694896
Manteiga <rafael.manteiga@hotmail.com>
2022-11-04 02:12:13 -0300

    Update 4.c

--- Lista 1/4.c
+++ Lista 1/4.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+
 int k;
-void leituradevetor(int v[10]){
-	
-	for(k=0;k<10;k++){
-		printf("Digite o número: ");
-		scanf("%d",&v[k]);
-	}
+void leituradevetor(int v[10]) {
+
+  for (k = 0; k < 10; k++) {
+    printf("Digite o número: ");
+    scanf("%d", & v[k]);
+  }
 }
 
-void print(int v[10]){
-	printf("");
-	for(k=0;k<10;k++){
-		printf("%d ",v[k]);
-	}
+void print(int v[10]) {
+  printf("");
+  for (k = 0; k < 10; k++) {
+    printf("%d ", v[k]);
+  }
 }
 
-void somar(int v[10]){
-	int soma=0;
-	for(k=0;k<10;k++){
-		soma = soma+v[k];
-	}
-	printf("%d",soma);
+void somar(int v[10]) {
+  int soma = 0;
+  for (k = 0; k < 10; k++) {
+    soma = soma + v[k];
+  }
+  printf("%d", soma);
 }
 
-int main(int argc, char *argv[]) {
-	
-	setlocale(LC_ALL,"portuguese");
-	
-	int v[10];
-	
-	leituradevetor(v);
-	printf("\n");
-	printf("Números do vetor: ");
-	print(v);
-	printf("\n");
-	printf("Soma dos números do vetor: ");
-	somar(v);
-	
+int main(int argc, char * argv[]) {
 
-	getch();
-	return 0;
+  setlocale(LC_ALL, "portuguese");
+
+  int v[10];
+
+  leituradevetor(v);
+  printf("\n");
+  printf("Números do vetor: ");
+  print(v);
+  printf("\n");
+  printf("Soma dos números do vetor: ");
+  somar(v);
+
+  getch();
+  return 0;
 }
