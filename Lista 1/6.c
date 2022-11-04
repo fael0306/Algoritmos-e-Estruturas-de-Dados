b0f3de9a52e59baf588d69d6fbee3fc625c675cd
Manteiga <rafael.manteiga@hotmail.com>
2022-11-04 02:14:14 -0300

    Update 6.c

--- Lista 1/6.c
+++ Lista 1/6.c
@@ -2,28 +2,28 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int multrec(int a, int b){
-	if(b==0){
-		return b;
-	}
-	return a+multrec(a,b-1);
+int multrec(int a, int b) {
+  if (b == 0) {
+    return b;
+  }
+  return a + multrec(a, b - 1);
 }
 
-int main(int argc, char *argv[]) {
-	
-	setlocale(LC_ALL,"portuguese");
-	
-	int a,b,r;
-	
-	printf("Digite o primeiro número: ");
-	scanf("%d",&a);
-	printf("\nDigite o segundo número: ");
-	scanf("%d",&b);
-	
-	r = multrec(a,b);
-	
-	printf("\n\nO resultado é: %d",r);
-	
-	getch();
-	return 0;
+int main(int argc, char * argv[]) {
+
+  setlocale(LC_ALL, "portuguese");
+
+  int a, b, r;
+
+  printf("Digite o primeiro nÃºmero: ");
+  scanf("%d", & a);
+  printf("\nDigite o segundo nÃºmero: ");
+  scanf("%d", & b);
+
+  r = multrec(a, b);
+
+  printf("\n\nO resultado Ã©: %d", r);
+
+  getch();
+  return 0;
 }
