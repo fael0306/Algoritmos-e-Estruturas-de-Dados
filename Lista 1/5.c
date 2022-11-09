9680d0b42dbaf53035c3d08b7b2668f20299649c
Manteiga <rafael.manteiga@hotmail.com>
2022-11-08 21:28:54 -0300

    Update 5.c

--- Lista 1/5.c
+++ Lista 1/5.c
@@ -7,7 +7,7 @@ bool primo(int n) {
 
   int k;
 
-  for (k = 2; k < n/2; k++) {
+  for (k = 2; k <= n / 2; k++) {
     if (n % k == 0) {
       return false;
     }
@@ -22,11 +22,11 @@ int main(int argc, char * argv[]) {
   int n, k;
 
   printf("Digite um número: ");
-  scanf("%d", &n);
+  scanf("%d", & n);
 
   printf("\nNúmeros primos até %d: ", n);
 
-  for (k = 2; k < n; k++) {
+  for (k = 2; k <= n; k++) {
     if (primo(k)) {
       printf("%d ", k);
     }
