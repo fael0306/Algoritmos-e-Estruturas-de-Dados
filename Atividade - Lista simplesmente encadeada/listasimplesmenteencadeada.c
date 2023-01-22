e4885c9ab151349622847be429da0efd5d202168
Manteiga <rafael.manteiga@hotmail.com>
2023-01-22 12:27:27 -0300

    Update listasimplesmenteencadeada.c

--- Atividade - Lista simplesmente encadeada/listasimplesmenteencadeada.c
+++ Atividade - Lista simplesmente encadeada/listasimplesmenteencadeada.c
@@ -1,3 +1,19 @@
+/*
+Faça um sistema para gerenciar os produtos (código, nome e preço) de um supermercado utilizando uma lista simplesmente encadeada, apresentando ao usuário as seguintes opções:
+
+  1- Cadastrar produto
+
+  2- Exibir a lista de produtos
+
+  3- Buscar um produto
+
+  4- Remover um produto
+
+  5- Sair
+
+Você deve implementar procedimentos e/ou funções para as funcionalidades de uma lista encadeada.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -31,5 +47,15 @@ int main() {
   insere(p, 2);
   insere(p, 10);
 
+  return 0;
+}
+int main() {
+  struct Lista lista1;
+
+  Lista * l = malloc(sizeof(Lista));
+
+  insere(l, 2);
+  insere(l, 10);
+  
   return 0;
 }
